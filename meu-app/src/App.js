528db4422417a2802e9785af432db075c115534d
MiguelBurati <127639123+MiguelBurati@users.noreply.github.com>
2026-01-21 11:52:32 -0300

    Update App.js

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import AddLivro from './components/AddLivro';
-import { useState } from 'react';
 
 
 function App() {
